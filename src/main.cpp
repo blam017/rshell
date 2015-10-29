206f7297e0ea0298aad648a40cd464aae90aed78
blam017 <blam017@ucr.edu>
2015-10-29 11:45:18 -0700

    No more exit.  Multiple ls works but echo is broken.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <sys/types.h>
-#include <boost/tokenizer.hpp>
+#include "connectors.h"
 
 using namespace std;
 
@@ -14,24 +11,12 @@ int main()
     //Gets user input until "exit" is typed
     while(1)
     {
-        cout << getlogin() << hostname  << "$ ";
+        cout << getlogin() << "@" << hostname  << "$ ";
         string userinput;
         getline(cin, userinput);
         
-
-        //Next 3 lines creates a Tokenizer which parses for characters.
-        typedef boost::tokenizer<boost::char_separator<char> > Tok;
-        boost::char_separator<char> sep;
-        Tok tok(userinput, sep);  //Container of tokens
-
-        for (Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end();
-            ++tok_iter)
-        {
-            if (*tok_iter == "exit")
-            {
-                exit(0);
-            }
-        }
+        separator(userinput);
     }
     return 0;
 }
+
