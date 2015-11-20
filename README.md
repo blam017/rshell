64e7d963f7a81f8a00abbfc580f3b916a7b56a05
blam017 <blam017@ucr.edu>
2015-11-20 07:38:11 -0800

    added precedence and test operations

--- README.md
+++ README.md
@@ -14,6 +14,20 @@ To use this shell, simply run the executable.  The program will then output
 a shell where you may type in commands as you would in a normal command prompt.
 Typing in the word "exit" exits the program and returns you back to terminal.
 
+#### UPDATE TO USAGE
+The new shell has added functionality which includes the "test" command and 
+precedence operators.
+
+### Design
+Unfortunately, this project was started with a messy and convoluted design and
+there has been very little to no time to fix it for now.  The entire project is
+based around functions which are called after a main loop finishes parsing 
+user input.  This made extensions for the project tedious, as brand new 
+functions had to be made in order to call existing ones for new operations such
+as the precedence operators.  Future changes to this program could include 
+switching the design pattern of the project from using disjoint functions
+to using classes so that adding onto and maintaining the code will be easier.
+
 ### Known bugs.
 
 1.  Possible memory leak when invalid commands are entered.
@@ -22,4 +36,7 @@ input instead of resetting. (Fixed)
 3.  The comment symbol, "#" does not work properly when placed at the back of
 a word.  For instance, "echo hello#world" prints "hello" when it is supposed
 to print "hello#world".
-
+4.  Quotes do not work as intended or have virtually no functionality at all.
+For example, echo "ls && a" should print ls && a.
+5.  Possible memory leak if stat() call fails.
+6.  Exit does not work if called within consecutive precedence operators.
--- src/execution.h
+++ src/execution.h
@@ -1,11 +1,13 @@
 #ifndef EXECUTION_H
 #define EXECUTION_H
 
+#include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <boost/tokenizer.hpp>
 #include <vector>
@@ -13,12 +15,190 @@
 
 using namespace std;
 
+void run_and (vector<string> &, bool&, bool, bool&);
+void run_or (vector<string> &, bool&, bool, bool&);
+void run_first (vector<string> &, bool&, bool&, bool&);
+
+
+bool run_precedence (vector<string> &commands, bool &is_first_command, bool &first_command_ran, bool &ex)
+{
+
+    commands.pop_back();
+    commands.erase(commands.begin());
+    queue< vector<string> > command_queue;
+    vector<string> commands_to_run;
+    int par_count = 0;
+    for (unsigned i = 0; i < commands.size(); ++i)
+    {
+        if (commands.at(i) == ";" && par_count == 0) // Check for ";" connector.
+        {
+            command_queue.push(commands_to_run);
+            commands_to_run.clear();
+        }
+        else if (commands.at(i) == "&&") // Check for "&&" connector.
+        {
+            commands_to_run.push_back("&&");
+        }
+        else if (commands.at(i) == "||") // Checks for "||" connector.
+        {
+            commands_to_run.push_back("||");
+        }
+        else if (commands.at(i) == "(")
+        {
+            par_count += 1;
+            commands_to_run.push_back("(");
+        }
+        else if (commands.at(i) == ")")
+        {
+            par_count -= 1;
+            commands_to_run.push_back(")");
+        }
+        else
+        {
+            commands_to_run.push_back(commands.at(i)); // Pushes input into vector
+        }
+    }
+    if (!commands_to_run.empty())
+    {
+        command_queue.push(commands_to_run);
+    }
+    
+    while (!command_queue.empty())
+    {
+        commands_to_run.clear();
+        is_first_command = true;
+        first_command_ran = true;
+
+        queue< vector<string> > run_queue;
+        par_count = 0;
+        for (unsigned i = 0; i < command_queue.front().size(); ++i)
+        {
+            
+            if (command_queue.front().at(i) == "(")
+            {
+                par_count += 1;
+            }
+            if (command_queue.front().at(i) == ")")
+            {
+                par_count -= 1;
+            }
+            
+
+            if (command_queue.front().at(i) != "&&" && 
+               command_queue.front().at(i) != "||" && par_count == 0)
+            {
+                commands_to_run.push_back(command_queue.front().at(i));
+            }
+            else if (par_count != 0)
+            {
+                commands_to_run.push_back(command_queue.front().at(i));
+            }
+            else
+            {
+                run_queue.push(commands_to_run);
+                commands_to_run.clear();
+                commands_to_run.push_back(command_queue.front().at(i));
+                run_queue.push(commands_to_run);
+                commands_to_run.clear();
+            }
+        }
+        if (!commands_to_run.empty())
+        {
+            run_queue.push(commands_to_run);
+        }
+        while (!run_queue.empty())
+        {
+            if (run_queue.front().at(0) == "&&")
+            {
+                run_queue.pop();
+                if (run_queue.front().at(0) == "exit" && first_command_ran)
+                {
+                    return false;
+                }
+                run_and (run_queue.front(), is_first_command, first_command_ran, ex);
+                run_queue.pop();
+            }
+            else if (run_queue.front().at(0) == "||")
+            {
+                run_queue.pop();
+                if (run_queue.front().at(0) == "exit" && !first_command_ran)
+                {
+                    return false;
+                }
+                run_or (run_queue.front(), is_first_command, first_command_ran, ex);
+                run_queue.pop();
+            }
+            else
+            {
+                if (run_queue.front().at(0) == "exit")
+                {
+                    return false;
+                }
+                run_first(run_queue.front(), is_first_command, first_command_ran, ex);
+                run_queue.pop();
+            }
+        }
+        command_queue.front().clear();
+        command_queue.pop();
+    }
+    return true;
+}
 //  This function runs based on the && connector.  It takes in a vector of 
 //commands to run along with 2 bools.  ran_first is the important one as it
 //detects whether or not the first command ran or not.
-void run_and (vector<string> &commands, bool &is_first, bool ran_first)
+void run_and (vector<string> &commands, bool &is_first, bool ran_first, bool &ex)
 {
-    if (ran_first)
+    if (commands.at(0) == "(" && ran_first)
+    {
+        //commands.erase(commands.begin());
+        ex = run_precedence (commands, is_first, ran_first, ex);
+    }
+    else if (ran_first && (commands.at(0) == "test" || commands.at(0) == "["))
+    {
+        ran_first = false;
+        if (commands.at(0) == "[") commands.pop_back();
+        commands.erase(commands.begin());
+        struct stat sb;
+        if (commands.at(0) == "-e" || commands.at(0) == "-d" ||
+            commands.at(0) == "-f")
+        {
+            if (stat(commands.at(1).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+        else
+        {
+            if (stat(commands.at(0).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+        if (commands.at(0) == "-e")
+        {
+            if(S_IFREG || S_IFDIR)
+            {
+                ran_first = true;
+            }
+        }
+        else if (commands.at(0) == "-d")
+        {
+            ran_first = S_IFDIR;
+        }
+        else if (commands.at(0) == "-f")
+        {
+            ran_first = S_IFREG;
+        }
+        else
+        {
+            if(S_IFREG || S_IFDIR)
+            {
+                ran_first = true;
+            }
+        }
+        
+    }
+    else if (ran_first)
     {
         int count = commands.size() + 1;
         char **args = new char*[count];
@@ -58,9 +238,58 @@ void run_and (vector<string> &commands, bool &is_first, bool ran_first)
 }
 
 //  Executes based on || connector.
-void run_or (vector<string> &commands, bool &is_first, bool ran_first)
+void run_or (vector<string> &commands, bool &is_first, bool ran_first, bool &ex)
 {
-    if (!ran_first)
+    if (commands.at(0) == "(" && !ran_first)
+    {
+        //commands.erase(commands.begin());
+        ex = run_precedence (commands, is_first, ran_first, ex);
+    }
+    else if (!ran_first && (commands.at(0) == "test" || commands.at(0) == "["))
+    {
+        ran_first = false;
+        if (commands.at(0) == "[") commands.pop_back();
+        commands.erase(commands.begin());
+        struct stat sb;
+        if (commands.at(0) == "-e" || commands.at(0) == "-d" ||
+            commands.at(0) == "-f")
+        {
+            if (stat(commands.at(1).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+        else
+        {
+            if (stat(commands.at(0).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+         if (commands.at(0) == "-e")
+        {
+            if(S_ISREG(sb.st_mode) || S_ISDIR(sb.st_mode))
+            {
+                ran_first = true;
+            }
+        }
+        else if (commands.at(0) == "-d")
+        {
+            ran_first = S_ISDIR(sb.st_mode);
+        }
+        else if (commands.at(0) == "-f")
+        {
+            ran_first = S_ISREG(sb.st_mode);
+        }
+        else
+        {
+            if(S_ISREG(sb.st_mode) || S_ISDIR(sb.st_mode))
+            {
+                ran_first = true;
+            }
+        }
+    }
+    else if (!ran_first)
     {
         int count = commands.size() + 1;
         char **args = new char*[count];
@@ -100,43 +329,95 @@ void run_or (vector<string> &commands, bool &is_first, bool ran_first)
 }
 
 //  First command or no connectors.
-void run_first(vector <string> &commands, bool &is_first, bool &ran_first)
+void run_first(vector <string> &commands, bool &is_first, bool &ran_first, bool &ex)
 {
-    int count = commands.size() + 1;
-    char **args = new char*[count];
-    for (int i = 0; i < count - 1; ++i)
+    if (commands.at(0) == "(")
     {
-        const char *mystr = commands.at(i).c_str();
-        args[i] = const_cast<char *> (&mystr[0]);
+        ex = run_precedence (commands, is_first, ran_first, ex);
     }
-    args[count - 1] = 0;
-    
-    int status;
-    pid_t c_pid, pid; // Where c_pid is child
-    c_pid = fork();
-    if (c_pid < 0)
+    else if (commands.at(0) == "test" || commands.at(0) == "[")
     {
-        perror("fork failed");
-        exit(1);
-    }
-    else if (c_pid == 0) // Child process
-    {
-        execvp(args[0], args);
-        perror("-bash");
-        exit(errno);
-    }
-    else if (c_pid > 0)
-    {
-        if ( (pid = wait(&status)) < 0)
+        ran_first = false;
+        if (commands.at(0) == "[") commands.pop_back();
+        commands.erase(commands.begin());
+        struct stat sb;
+        if (commands.at(0) == "-e" || commands.at(0) == "-d" ||
+            commands.at(0) == "-f")
         {
-            perror("wait");
+            if (stat(commands.at(1).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+        else
+        {
+            if (stat(commands.at(0).c_str(), &sb) == -1)
+            {
+                perror("stat");
+            }
+        }
+
+        if (commands.at(0) == "-e")
+        {
+            if(S_ISREG(sb.st_mode) || S_ISDIR(sb.st_mode))
+            {
+                ran_first = true;
+            }
+        }
+        else if (commands.at(0) == "-d")
+        {
+            ran_first = S_ISDIR(sb.st_mode);
+        }
+        else if (commands.at(0) == "-f")
+        {
+            ran_first = S_ISREG(sb.st_mode);
+        }
+        else
+        {
+            if(S_ISREG(sb.st_mode) || S_ISDIR(sb.st_mode))
+            {
+                ran_first = true;
+            }
+        }
+    }
+    else
+    {
+        int count = commands.size() + 1;
+        char **args = new char*[count];
+        for (int i = 0; i < count - 1; ++i)
+        {
+            const char *mystr = commands.at(i).c_str();
+            args[i] = const_cast<char *> (&mystr[0]);
+        }
+        args[count - 1] = 0;
+        
+        int status;
+        pid_t c_pid, pid; // Where c_pid is child
+        c_pid = fork();
+        if (c_pid < 0)
+        {
+            perror("fork failed");
             exit(1);
         }
-        if (WEXITSTATUS(status) != 0)
+        else if (c_pid == 0) // Child process
         {
-            ran_first = false; // True if execvp executes, else false.
+            execvp(args[0], args);
+            perror("-bash");
+            exit(errno);
         }
-        delete [] args;
+        else if (c_pid > 0)
+        {
+            if ( (pid = wait(&status)) < 0)
+            {
+                perror("wait");
+                exit(1);
+            }
+            if (WEXITSTATUS(status) != 0)
+            {
+                ran_first = false; // True if execvp executes, else false.
+            }
+            delete [] args;
+        }
     }
     is_first = false;
 }
@@ -148,17 +429,19 @@ bool execution (string userinput)
 
     // Tokenizer which searches for connectors
     typedef boost::tokenizer<boost::char_separator<char> > Tok;
-        boost::char_separator<char> sep(" ", ";#|&", boost::drop_empty_tokens);
+        boost::char_separator<char> sep(" ", ";#|&()[]", boost::drop_empty_tokens);
         Tok tok(userinput, sep);  //Container of tokens
 
+    int par_count = 0, bracket_count = 0;    
     for (Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end();
         ++tok_iter)
-    {   
+    {  
+
         if (*tok_iter == "#")
         {
             break; // If # ignore rest.
         }
-        else if (*tok_iter == ";") // Check for ";" connector.
+        else if (*tok_iter == ";" && par_count == 0) // Check for ";" connector.
         {
 
             Tok::iterator check = tok_iter;
@@ -183,7 +466,7 @@ bool execution (string userinput)
 
             Tok::iterator check = tok_iter;
             ++check;
-            if (check == tok.end())
+            if (check == tok.end() || *check == ")")
             {
                 cout << "Error: No command after connector.\n";
                 return true;
@@ -207,7 +490,7 @@ bool execution (string userinput)
 
             Tok::iterator check = tok_iter;
             ++check;
-            if (check == tok.end())
+            if (check == tok.end() || *check == ")")
             {
                 cout << "Error: No command after connector.\n";
                 return true;
@@ -219,6 +502,35 @@ bool execution (string userinput)
             }
             commands.push_back("||");
         }
+        else if (*tok_iter == "(")
+        {
+            par_count += 1;
+            commands.push_back(*tok_iter);
+        }
+        else if (*tok_iter == ")")
+        {
+            par_count -= 1;
+            Tok::iterator check = tok_iter;
+            ++check;
+            if (check != tok.end() && *check != "|" && *check != "&" && 
+                    *check != ";" && par_count == 0)
+            {
+                cout << "-bash: Syntax error: Unexpected token near \")\"\n";
+                return true;
+            }
+            commands.push_back(*tok_iter);
+            //cout << commands.at(commands.size() - 1) << endl;
+        }
+        else if (*tok_iter == "[")
+        {
+            bracket_count += 1;
+            commands.push_back(*tok_iter);
+        }
+        else if (*tok_iter == "]")
+        {
+            bracket_count -= 1;
+            commands.push_back(*tok_iter);
+        }
         else
         {
             commands.push_back(*tok_iter); // Pushes input into vector
@@ -227,6 +539,11 @@ bool execution (string userinput)
     if (!commands.empty())
     {
         command_queue.push(commands);
+    }
+    if(par_count != 0 || bracket_count != 0)
+    {
+        cout << "-bash: Syntax error: Number of operators do not match\n";
+        return true;
     }
 
     while (!command_queue.empty()) // Execution loop after parsing commands.
@@ -244,12 +561,27 @@ bool execution (string userinput)
             return true;
         }
 
+        // PRECEDENCE MIGHT GO HERE
+
         vector<string> commands_to_run;
         queue< vector<string> > run_queue;
+        int par_count = 0;
         for (unsigned i = 0; i < command_queue.front().size(); ++i)
         {
+            if (command_queue.front().at(i) == "(")
+            {
+                par_count += 1;
+            }
+            if (command_queue.front().at(i) == ")")
+            {
+                par_count -= 1;
+            }
             if (command_queue.front().at(i) != "&&" && 
-               command_queue.front().at(i) != "||")
+               command_queue.front().at(i) != "||" && par_count == 0)
+            {
+                commands_to_run.push_back(command_queue.front().at(i));
+            }
+            else if (par_count != 0)
             {
                 commands_to_run.push_back(command_queue.front().at(i));
             }
@@ -272,7 +604,7 @@ bool execution (string userinput)
             cout << "-bash: Syntax error: Connector at end of argument.\n";
             return true;
         }
-
+        bool ex = true;
         while (!run_queue.empty())
         {
             if (run_queue.front().at(0) == "&&")
@@ -282,8 +614,9 @@ bool execution (string userinput)
                 {
                     return false;
                 }
-                run_and (run_queue.front(), is_first_command, first_command_ran);
+                run_and (run_queue.front(), is_first_command, first_command_ran, ex);
                 run_queue.pop();
+                if(!ex) return false;
             }
             else if (run_queue.front().at(0) == "||")
             {
@@ -292,13 +625,16 @@ bool execution (string userinput)
                 {
                     return false;
                 }
-                run_or (run_queue.front(), is_first_command, first_command_ran);
+                run_or (run_queue.front(), is_first_command, first_command_ran, ex);
                 run_queue.pop();
+                if(!ex) return false;
             }
+            else if (!ex)   return false;
             else
             {
-                run_first(run_queue.front(), is_first_command, first_command_ran);
+                run_first(run_queue.front(), is_first_command, first_command_ran, ex);
                 run_queue.pop();
+                if(!ex) return false;
             }
         }
         
